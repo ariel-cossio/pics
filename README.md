be7cef9e4d261f2207fa559760997cc5a36557c8
Nestor Felipe <necrifede@gmail.com>
2014-11-26 21:43:32 -0400

    create api to get and add Content

--- README.md
+++ README.md
@@ -1,38 +1,35 @@
-+# Store Pictures Application
-+
-+This application plans to store images, Using sinatra application providing a
-+REST API to images tree model
-+
-+# REST API
-+ 
-+The REST API to the example app is described below.
-+
-+## Create a user account
-+ 
-+I should get logged into the application
-+
-+
-+
-+
-+
-+## Add Pictures and folders
-+
-+I should be able to store images in the cloud
-+
-+
-+
-+
-+
-+
-+## See the elements by folder
-+
-+It should be possible Pictures and other folder
-+
-+
-+
-+
-+
-+
-+
-+
+# PICS - PICS images confident store
+
+This application store images, Using sinatra application providing a
+REST API to images tree model
+
+## Requirements
+
+* Install ImageMagic and RMagick gem:
+	http://shoobm.wordpress.com/tag/imagemagick/
+
+
+# REST API
+ 
+The REST API to the example app is described below.
+
+## Create a user account
+ 
+I should get logged into the application
+
+
+
+## See the elements by folder
+
+It should be possible Pictures and other folder
+
+
+## See a full image
+
+It should be possible retrieve the full value of an stored image
+
+
+## Add Pictures and folders
+
+I should be able to store images in the cloud
 
--- app/app.rb
+++ app/app.rb
@@ -113,3 +113,186 @@ post '/secure/add_folder' do
 end
 
 
+
+require 'sinatra/reloader' if development?
+require 'json'
+require 'base64'
+require 'fileutils'
+require 'rubygems'
+require 'RMagick'
+include Magick
+
+
+#################
+# Model section #
+#################
+
+class FSElement
+  attr_accessor :name
+
+  def initialize  name
+    @name = name
+  end
+
+end
+
+class ElemFolder < FSElement
+
+  attr_accessor :element_list, :type_name
+  @@type_name = "folder"
+
+  def initialize name
+    super(name)
+    @element_list = Array.new
+  end
+  
+  # Add a Folder for the current folder
+  # Params:
+  # +name+:: name of folder to be added
+  def add_folder name
+    elem = ElemFolder.new(name)
+    @element_list.push(elem)
+  end
+
+  # Add an Image for this folder
+  # Params:
+  # +name+:: name of image to be added
+  # +data+:: data file name encoded using base 64
+  def add_image(name, data)
+    elem = ElemImage.new(name, data)
+    @element_list.push(elem)
+  end
+
+  # Remove an element of current folder given the name
+  # Params:
+  # +name+:: item name to be removed
+  def remove_element name
+    @element_list.delete_if{|e|
+      e.name == name
+    }
+  end
+
+  # Return the list of elements: folders and images
+  # Params:
+  # +path+:: folders path
+  def list_elements path
+    return @element_list
+  end
+
+  # Return the name of the type value of this class
+  def self.type_name
+    @@type_name
+  end
+end
+
+class ElemImage < FSElement
+  attr_accessor :data, :preview
+  @@type_name = "image"
+
+  # Create a new element image
+  # Params:
+  # +name+:: image name file
+  # +data+:: raw data encoded in base64 format
+  def initialize(name, data)
+    super(name)
+    @data = data
+    @random_generator = Random.new(12345)
+    set_preview
+  end
+
+  # Set a thumbnail for the current image
+  def set_preview
+    temporal = get_random_dir()
+    FileUtils::mkdir_p temporal
+
+    file = File.open("./#{temporal}/#{@name}", 'wb') do|f|
+        f.write(Base64.decode64(@data))
+    end
+
+    img = ImageList.new("./#{temporal}/#{@name}")
+    width, height = 50, 50
+    thumb = img.scale(width, height)
+    thumb.write("./#{temporal}/thumb_#{@name}")
+
+    f_thumb = File.open("./#{temporal}/thumb_#{@name}", 'rb').read
+    encoded_string = Base64.encode64(f_thumb)
+
+    @preview = encoded_string
+    FileUtils.rm_rf('./#{temporal}')
+  end
+
+  # Generate a random dir name
+  def get_random_dir
+    random_folder = @random_generator.rand(1000000)
+    return random_folder.to_s
+  end
+
+  # Return the name of the type value of this class
+  def self.type_name
+    @@type_name
+  end
+
+  private :set_preview, :get_random_dir
+
+end
+
+class User
+  attr_accessor :username
+
+  def initialize usrname
+    @username = name
+    @root_folder = ElemFolder.new
+  end
+end
+
+
+#########################
+# Rest Services Section #
+#########################
+
+
+root = ElemFolder.new '/'
+
+#set :port, 8080
+
+# Retrieve the content of the current folder
+#
+get '/api/content' do
+  return_message = []
+  list = root.list_elements '/'
+  list.map{|elem| 
+    item = {}
+    item['name'] = elem.name
+    item['type'] = elem.class.type_name
+    if item['type'] == 'image'
+      item['preview'] = elem.preview
+    end
+    return_message.push(item)
+    item
+  }
+  return_message.to_json
+end
+
+# Add a content to the server, This content could be an image or a 
+# folder
+post '/api/add/content' do
+
+  return_message = {} 
+  print(params[:data])
+  jdata = JSON.parse(params[:data],:symbolize_names => true) 
+  if jdata.has_key?(:name) 
+    if jdata[:type] == "image"
+      root.add_image(jdata[:name], jdata[:data])
+    elsif jdata[:type] == "folder"
+      root.add_folder(jdata[:name])
+    else
+      status 500
+    end
+    return_message[:status] = 'succeed'
+  else
+    return_message[:status] = 'unable to add - missing data'
+  end
+  return_message.to_json 
+end
+
+
