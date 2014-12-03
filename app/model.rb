7466e9c05aff0493c14ee3e48ca4fba962e9d786
Ariel Cossio <ariel.cossio@gmail.com>
2014-12-03 08:11:21 -0400

    Updated model according to requirements of the Rest API

--- app/model.rb
+++ app/model.rb
@@ -7,39 +7,102 @@ configure do
   Mongoid.load!("./mongoid.yml")
 end
 
-class ImageUploader < CarrierWave::Uploader::Base
-  storage :file
+#################
+# Model section #
+#################
+
+class DuplicateElementException < Exception
 end
+
 
 class FSElement
-	include Mongoid::Document
-	belongs_to :to_folder, :class_name => 'Folder', :inverse_of => :fselements
-	field :path
-	field :name, type: String
-	field :created
-	validates_presence_of :path
-	validates_presence_of :name
-	validates_presence_of :created
+  include Mongoid::Document
+  belongs_to :to_folder, :class_name => 'ElemFolder', :inverse_of => :fselements
+  field :name, type: String
+  field :type_name, type: String
+  validates_presence_of :name
+  validates_presence_of :type_name
 end
 
-class Image < FSElement
-	include Mongoid::Document
-	mount_uploader :image, ImageUploader, type: String
-	field :alias, type: String
-	field :size
-	validates_presence_of :size
+class ElemFolder < FSElement
+  include Mongoid::Document
+  include Comparable
+  belongs_to :to_user, :class_name => 'User', :inverse_of => :folder
+  has_many :fselements, :class_name => 'FSElement', :inverse_of => :to_folder
+  type_name = "folder"
+
+  # Add a Folder for the current folder
+  # Params:
+  # +name+:: name of folder to be added
+  def add_folder name
+    elem = ElemFolder.create
+    elem.name = name
+    add_element(elem)
+  end
+
+  # Add an Image for this folder
+  # Params:
+  # +name+:: name of image to be added
+  # +data+:: data file name encoded using base 64
+  def add_image(name, data)
+    elem = ElemImage.create
+    elem.name = name
+    elem.data = data
+    add_element(elem)
+  end
+
+  def add_element(element)
+  	element_found = fselements.where(name: element.name)
+  	if element_found
+    	if element_found.type_name == element.type_name
+      	raise(DuplicateElementException,
+        	    "#{element.class.type_name} '#{element.name}' already exist")
+    	end
+    end
+    fselements.push(element)
+  end
+
+  # Remove an element of current folder given the name
+  # Params:
+  # +name+:: item name to be removed
+  def remove_element(name)
+    fselements.where(name: name).destroy_all
+  end
+
+  # Return the list of elements: folders and images
+  # Params:
+  # +path+:: folders path
+  def list_elements(path)
+    return fselements
+  end
+
+  private :add_element
 end
 
-class Folder < FSElement
-	include Mongoid::Document
-	belongs_to :to_user, :class_name => 'User', :inverse_of => :folder
-	has_many :fselements, :class_name => 'FSElement', :inverse_of => :to_folder
+
+class ElemImage < FSElement
+  include Mongoid::Document
+  include Comparable
+
+  field :data, type: String
+  field :preview, type: String
+  type_name = "image"
+
+  @random_generator = Random.new(12345)
+
+  # Generate a random dir name
+  def get_random_dir()
+    random_folder = @random_generator.rand(1000000)
+    return random_folder.to_s
+  end
+
+  private :get_random_dir
+
 end
 
 class User
-	include Mongoid::Document
-	has_one :folder, :class_name => 'Folder', :inverse_of => :to_user
-	field :user_name, type: String
-	field :password, type: String
-	validates_presence_of :user_name
+  include Mongoid::Document
+  has_one :folder, :class_name => 'ElemFolder', :inverse_of => :to_user
+  field :user_name
+  validates_presence_of :user_name
 end
