c1437269e21e2c995681c181558865f6e944ac7d
Ariel Cossio <ariel.cossio@gmail.com>
2014-12-09 19:57:06 -0400

    Integrated user model

--- Gemfile
+++ Gemfile
@@ -2,11 +2,12 @@ source "https://rubygems.org"
 
 gem 'json'
 gem 'rest-client'
-gem 'sinatra'
-gem 'mongoid'
-gem 'carrierwave'
-gem 'carrierwave-mongoid'
-gem 'mongoid-rspec', '~> 2.0.0.rc1'
+gem 'rmagick', '2.13.2'
+gem 'sinatra', '1.3.4'
+gem 'sinatra-flash', '0.3.0', require: 'sinatra/flash'
+gem 'data_mapper', '1.2.0'
+gem 'dm-sqlite-adapter', '1.2.0'
+gem 'warden', '1.2.1'
 
 group :development do
   gem 'rerun'
--- app/model.rb
+++ app/model.rb
@@ -1,3 +1,40 @@
+require 'data_mapper'
+require 'dm-sqlite-adapter'
+require 'bcrypt'
+
+DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db.sqlite")
+
+class User
+  include DataMapper::Resource
+  include BCrypt
+
+  property :id, Serial, key: true
+  property :username, String, length: 128
+
+  property :password, BCryptHash
+
+  def authenticate(attempted_password)
+    if self.password == attempted_password
+      true
+    else
+      false
+    end
+  end
+end
+
+# Tell DataMapper the models are done being defined
+DataMapper.finalize
+
+# Update the database to match the properties of User.
+DataMapper.auto_upgrade!
+
+# Create a test User
+if User.count == 0
+  @user = User.create(username: "admin")
+  @user.password = "admin"
+  @user.save
+end
+
 #require 'sinatra'
 #require 'mongoid'
 #require 'carrierwave'
@@ -218,18 +255,3 @@ class ElemImage < FSElement
   private :set_preview, :get_random_dir
 
 end
-
-
-class User
-  attr_accessor :username
-
-  def initialize usrname
-    @username = name
-    @root_folder = ElemFolder.new '/'
-  end
-
-  def get_root_folder()
-    return @root_folder
-  end
-end
-
